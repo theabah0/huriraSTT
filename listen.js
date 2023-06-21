2a60e8b5bc860ff62a1575124f804415a9a60c23
theabah0 <130188984+theabah0@users.noreply.github.com>
2023-06-21 12:00:35 +0300

    Update listen.js

--- listen.js
+++ listen.js
@@ -1,7 +1,31 @@
-if ("webkitSpeechRecognition" in window) {
+document.addEventListener('DOMContentLoaded', function() {
+  
 
+
+if ("webkitSpeechRecognition" in window) {
+//animated icon
+/*
+const icon = document.querySelector("#myIcon");
+
+function handleClick() {
+  icon.classList.add("clicked");
+}
+
+icon.addEventListener("click", handleClick);
+
+*/
+//indent functionality
+document.querySelector("#indent").addEventListener("click",()=>{
+    document.querySelector("#chatContainer").appendChild(document.createElement('br'))
+});
+
+//scrolling to bottom
 function scrollToBottom() { const messagesContainer = document.querySelector('.innerBody'); messagesContainer.scrollTop = messagesContainer.scrollHeight; }
 
+//show/hide settings
+document.querySelector(".navbar-right").addEventListener("click",()=>{
+    settingToggle()
+})
 function settingToggle(){
 let settings = document.querySelector('.settings')
 if (settings.style.display=="none"){
@@ -12,11 +36,12 @@ if (settings.style.display=="none"){
     
 }
    // chat functionality 
-    chatContainer= document.querySelector("#chatContainer")
+    chatContainer= document.querySelector("#chatContainer");
      function createUserMessage(interim_transcript) {
   const chatBubble = document.createElement('div'); 
 chatBubble.classList.add('text-light'); chatBubble.classList.add('chat-bubble');
-  chatBubble.innerText = interim_transcript;
+//make first letter of capital and add full stop.
+  chatBubble.innerText = interim_transcript.charAt(0).toUpperCase() + interim_transcript.slice(1) +". ";
   chatContainer.appendChild(chatBubble);
 } 
 //web speech api config and setting parameters
@@ -28,10 +53,10 @@ chatBubble.classList.add('text-light'); chatBubble.classList.add('chat-bubble');
   speechRecognition.lang = document.querySelector("#select_dialect").value;
 //on start 
   speechRecognition.onstart = () => {
-    document.querySelector("#status").style.display = "inline-block";
+    document.querySelector("#status").innerHTML= "Listening ...";
   };
   speechRecognition.onerror = () => {
-    document.querySelector("#status").style.display = "none";
+    document.querySelector("#status").innerText = "error";
     console.log("Speech Recognition Error");
   };
   //on result
@@ -39,32 +64,72 @@ chatBubble.classList.add('text-light'); chatBubble.classList.add('chat-bubble');
     let interim_transcript = "";
 
     for (let i = event.resultIndex; i < event.results.length; ++i) {
-    //  if (event.results[i].isFinal) {
-     /*   final_transcript += event.results[i][0].transcript;
-      } else {*/
+   
        interim_transcript += event.results[i][0].transcript;
        
-     // }
+     
     }
  
   //on end
   speechRecognition.onend = () => {
-    document.querySelector("#status").style.display = "none";
+    document.querySelector("#status").innerText = "Tap fly to talk";
     
   document.querySelector("#interim").innerText=""; createUserMessage(interim_transcript);
 scrollToBottom();
     console.log("Speech Recognition Ended");
   };
 //createUserMessage(interim_transcript)
- //document.querySelector("#final").innerHTML = final_transcript;
-    document.querySelector("#interim").innerHTML = interim_transcript;
+ 
+    document.querySelector("#interim").innerHTML = interim_transcript ;
   };
 
+ //ignition 
   document.querySelector("#start").onclick = () => {
     speechRecognition.start();
   };
-
+  
+  
+  //copy to clipboard functionality
+  
+  /* getting the text*/
+ var textToCopy =''; 
+  console.log(textToCopy);
+const copyButton = document.getElementById('copyButton');
+  copyButton.addEventListener('click', copyToClipboard);
+function show(mess){
+    document.querySelector('#cliperr').innerHTML=mess;
+}
+  function copyToClipboard() {
+    
+[...document.querySelector("#chatContainer").children].forEach((child)=>{
+      textToCopy+=child.innerText;
+  } );
+  console.log(textToCopy);
+    try {
+      navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
+        if (result.state === 'granted' || result.state === 'prompt') {
+          navigator.clipboard.writeText(textToCopy)
+            .then(() => {
+              show('Text copied to clipboard');
+              // You can display a success message or perform other actions here
+            })
+            .catch(err => {
+              show('Failed to copy text: ', err);
+              show('DOMException: ', err.name);
+              // Display an error message or handle the error in an appropriate way
+            });
+        } else {
+          show('Clipboard write permission denied');
+          // Display an error message or handle the error in an appropriate way
+        }
+      });
+    } catch (err) {
+      show('Clipboard API not supported', err);
+      // Display an error message or handle the error in an appropriate way
+    }
+  }
   
 } else {
   alert("Speech Recognition Not Available");
 }
+});
